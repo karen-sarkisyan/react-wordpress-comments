23a44f122580bfee5b71df779591b7cf2d8210a9
karen-sarkisyan <karen.sarkisyan01@gmail.com>
2021-03-14 19:33:13 +0300

    Some initial project setup

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,24 +1,22 @@
-import React, { Component } from "react"
+import React from "react"
 import { render } from "react-dom"
 
 import WpComments from "../../src"
 import "../../src/styles.css"
 
-export default class Demo extends Component {
-  render() {
-    return (
-      <div>
-        <h1>react-wordpress-comments Demo</h1>
-        <WpComments
-          maxDepth={3}
-          pageId={619}
-          hostUrl="http://localhost:8888/localsite_new3"
-          allowComments={true}
-          user={null}
-        />
-      </div>
-    )
-  }
+function Demo() {
+  return (
+    <div>
+      <h1>react-wordpress-comments Demo</h1>
+      <WpComments
+        maxDepth={3}
+        pageId={2}
+        hostUrl="http://wordpress-for-fun.000webhostapp.com"
+        allowComments={true}
+        user={null}
+      />
+    </div>
+  )
 }
 
 render(<Demo />, document.querySelector("#demo"))
